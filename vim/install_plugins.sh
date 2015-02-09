cc50e991bb2f1156ae25583d2870c390ee4e7a90
Mikael Andersson <qubex2@gmail.com>
2015-02-10 00:38:33 +0100

    added install plugins script

---
+++ vim/install_plugins.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+git clone https://github.com/mileszs/ack.vim.git bundle/ack.vim
+git clone https://github.com/kien/ctrlp.vim.git bundle/ctrlp.vim
+git clone https://github.com/scrooloose/nerdtree.git bundle/nerdtree
+git clone git://github.com/shawncplus/phpcomplete.vim.git bundle/phpcomplete.vim
+git clone https://github.com/ervandew/supertab.git bundle/supertab
+git clone git://github.com/majutsushi/tagbar bundle/tagbar
+git submodule add git://github.com/SirVer/ultisnips.git bundle/ultisnips
+git clone https://github.com/bling/vim-airline bundle/vim-airline
+
