808884fb3a37baa798d513895e6a4cfa5a9acd71
Mikael Andersson <qubex2@gmail.com>
2015-02-10 01:01:19 +0100

    updated emacs install script

--- emacs.d/install_plugins.sh
+++ emacs.d/install_plugins.sh
@@ -2,6 +2,6 @@
 if [ ! -d bundle ]; then
     mkdir bundle
     git clone https://github.com/hbin/molokai-theme.git
-    git clone git@github.com:fxbois/web-mode.git
+    git clone https://github.com/fxbois/web-mode.git
 fi
 
