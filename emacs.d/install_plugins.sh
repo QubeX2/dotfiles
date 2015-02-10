0552701dab05d97cba3684c20689de580a3f641e
Mikael Andersson <qubex2@gmail.com>
2015-02-10 01:03:12 +0100

    fixed error in emacs install script

--- emacs.d/install_plugins.sh
+++ emacs.d/install_plugins.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ ! -d bundle ]; then
     mkdir bundle
-    git clone https://github.com/hbin/molokai-theme.git
-    git clone https://github.com/fxbois/web-mode.git
+    git clone https://github.com/hbin/molokai-theme.git bundle/molokai-theme
+    git clone https://github.com/fxbois/web-mode.git bundle/web-mode
 fi
 
