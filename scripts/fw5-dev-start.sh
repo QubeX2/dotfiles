c803e8ccabaa5549e221c971b1f04ba3eddb7787
Mikael Andersson <qubex2@gmail.com>
2019-06-19 12:12:35 +0200

    updated

--- scripts/fw5-dev-start.sh
+++ scripts/fw5-dev-start.sh
@@ -1,36 +1,36 @@
 #!/bin/sh
 
 if [ "$1" == "start" ]; then
-    if [ -f /tmp/laravel.pid ]; then
+    if [ -f /tmp/fw5-les.pid ]; then
         echo "Already started..."
     else
         cd ~/Sites/fwork5
         laravel-echo-server start &
-        echo $! > /tmp/laravel.pid
+        echo $! > /tmp/fw5-les.pid
 
         php artisan fwork:telavox &
-        echo $! > /tmp/telavox.pid
+        echo $! > /tmp/fw5-telavox.pid
 
         yarn run watch &
-        echo $! > /tmp/yarn.pid
+        echo $! > /tmp/fw5-yarn.pid
     fi
 
 elif [ "$1" == "stop" ]; then
-    if [ -f /tmp/laravel.pid ]; then
-        kill `cat /tmp/laravel.pid`
-        rm /tmp/laravel.pid
+    if [ -f /tmp/fw5-les.pid ]; then
+        kill `cat /tmp/fw5-les.pid`
+        rm /tmp/fw5-les.pid
     fi
-    if [ -f /tmp/telavox.pid ]; then
-        kill `cat /tmp/telavox.pid`
-        rm /tmp/telavox.pid
+    if [ -f /tmp/fw5-telavox.pid ]; then
+        kill `cat /tmp/fw5-telavox.pid`
+        rm /tmp/fw5-telavox.pid
     fi
-    if [ -f /tmp/yarn.pid ]; then
-        kill `cat /tmp/yarn.pid`
-        rm /tmp/yarn.pid
+    if [ -f /tmp/fw5-yarn.pid ]; then
+        kill `cat /tmp/fw5-yarn.pid`
+        rm /tmp/fw5-yarn.pid
     fi
 else
 
-    echo "Usage: fw5-dev-start [start|stop]"
+    echo "Usage: fw5-init [start|stop]"
 fi
 
 
