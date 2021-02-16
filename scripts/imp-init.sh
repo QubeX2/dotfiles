cece2ffc5f8ce61e9e7fe1a1dced765a8be7af79
Mikael Andersson <qubex2@gmail.com>
2021-02-16 07:48:43 +0100

    updated

--- scripts/imp-init.sh
+++ scripts/imp-init.sh
@@ -1,24 +1,22 @@
 #!/bin/sh
-n 13.10.1
+n 14
 if [ "$1" == "start" ]; then
-    if [ -f /tmp/gs-les.pid ]; then
+    if [ -f /tmp/imp-les.pid ]; then
         echo "Already started..."
     else
-        cd ~/Sites/golf-service-system
-        laravel-echo-server start &
-        echo $! > /tmp/gs-les.pid
-
-        npm run watch-poll &
+        cd ~/Sites/impecta
+        yarn run watch &
+        echo $! > /tmp/imp-les.pid
     fi
 
 elif [ "$1" == "stop" ]; then
-    if [ -f /tmp/gs-les.pid ]; then
-        kill `cat /tmp/gs-les.pid`
-        rm /tmp/gs-les.pid
+    if [ -f /tmp/imp-les.pid ]; then
+        kill `cat /tmp/imp-les.pid`
+        rm /tmp/imp-les.pid
     fi
 else
 
-    echo "Usage: gs-init [start|stop]"
+    echo "Usage: imp-init [start|stop]"
 fi
 
 
